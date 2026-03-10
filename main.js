c57b8abc2266b25dce1a6f2069e1bee66d162ae7
Torculas9Say2526 <torculas.fd@obmontessori.edu.ph>
2026-03-10 08:46:57 +0800

    Add code
    Add function to calculate ticket prices with admin fee.

---
+++ main.js
@@ -0,0 +1,10 @@
+function verbNoun() {
+    // variable decleration
+    const ticket_Price = 250.00;
+    const ticket_AdminFee = 50.00;
+    let ticket_Quantity = Number(document.getElementById("ticket-quantity").value);
+    let subTotal = Number(12.50 * ticket_Quantity);
+    let totalPrice = Numver(subTotal + ticket_AdminFee);
+    value = 0
+    min = 0
+}
